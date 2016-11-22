95743be3c02337d56b4386f15dcd184c2f96638f
cmniccum <cmniccum@siu.edu>
2016-11-22 11:17:50 -0600

    Created Main

--- src/LexerToken.java
+++ src/LexerToken.java
@@ -2,5 +2,10 @@
  */
 
 public enum LexerToken {
-
+	
+	H1, H2, H3, H4, H5, H6,		// headers
+	HR,							// horizontal rule
+	ITAL, BOLD, CODE, STRIKE,	// emphasis
+	OL, IL,						// lists
+	C_BLOCK, Q_BLOCK			// blocks
 }
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,33 @@
+import java.io.*;
+
 public class Main {
 
+	 public static void main(String[] args) throws IOException {
+		 
+		String template;
+		
+		template = args[1];
+		String outputfile = null;
+		
+		int i = 0;
+		while(template.charAt(i) != '.'){
+			outputfile = outputfile + template.charAt(i);
+			i++;
+		}
+		BufferedReader infile = new BufferedReader(new FileReader(args[1]));
+		PrintWriter outfile = new PrintWriter(new FileWriter(outputfile + ".html"));
+		
+		 String line;
+		 
+		 while((line = infile.readLine()) != null){
+			 
+			 
+			 
+		 }
+		 
+		 
+		 
+		 
+		 
+	 }
 }
