37fd052096d29ea4a069162bcd03543e9cea01af
MRBeussink <markbeussink@wlcl091223.wlcl.siu.edu>
2016-11-29 13:35:26 -0600

    Update LexerToken
    Changed names and added tokens for Links and Images

--- src/LexerToken.java
+++ src/LexerToken.java
@@ -1,11 +1,27 @@
 /* This enum contains tokens for the lexer
  */
 
+
 public enum LexerToken {
-	
-	H1, H2, H3, H4, H5, H6,		// headers
-	HR,							// horizontal rule
-	ITAL, BOLD, CODE, STRIKE,	// emphasis
-	OL, IL,						// lists
-	C_BLOCK, Q_BLOCK			// blocks
+
+    /*
+        * headers
+        * horizontal rule
+        * emphasis/code
+        * lists
+        * blocks
+        * Links
+        * Images
+        * Tables?
+        * New Line
+     */
+
+    Header,
+    HrzRule,
+    Ital, Bold, Strike, Code,
+    UList, OList,
+    CBlock, QBlock,
+    LinkOpen, LinkClose,
+    ImgOpen, ImgClose, Split,
+    NewLine
 }
