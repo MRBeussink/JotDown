65c35d45950a4972fdc1cde4b76cd83de6174591
MRBeussink <MRBeussink@gmail.com>
2016-12-05 07:35:45 -0600

    Fleshed out parsing, addition to LexerToken
    Parser now parses lexemes, still needs to translate.
    
    Added getValue() to all LexerToken, the default is now returning the
    special character unless the user input is needed.

--- src/LexerToken.java
+++ src/LexerToken.java
@@ -20,19 +20,57 @@ public enum LexerToken {
     	@Override public void setValue(String value) { this.value = value; }
     	@Override public String getValue() { return this.value; }
     },
-    HrzRule,
-    Ital, Bold, Strike, Code,
-    UList, OList,
-    CBlock, QBlock,
-    LinkOpen, LinkClose,
-    ImgOpen, ImgClose, Split,
-    NewLine,
+    HrzRule {
+        @Override public String getValue() { return "***"; }
+    },
+    Ital {
+        @Override public String getValue() { return "_"; }
+    },
+    Bold {
+        @Override public String getValue() { return "*"; }
+    },
+    Strike {
+        @Override public String getValue() { return "-"; }
+    },
+    Code {
+        @Override public String getValue() { return "`"; }
+    },
+    UList {
+        @Override public String getValue() { return "* "; }
+    },
+    OList {
+        @Override public String getValue() { return "#. "; }
+    },
+    CBlock {
+        @Override public String getValue() { return "```"; }
+    },
+    QBlock {
+        @Override public String getValue() { return ">>"; }
+    },
+    LinkOpen {
+        @Override public String getValue() { return "["; }
+    },
+    LinkClose {
+        @Override public String getValue() { return "]"; }
+    },
+    ImgOpen {
+        @Override public String getValue() { return "{"; }
+    },
+    ImgClose {
+        @Override public String getValue() { return "}"; }
+    },
+    Split {
+        @Override public String getValue() { return ":"; }
+    },
+    NewLine {
+        @Override public String getValue() { return "\n"; }
+    },
     Text {
     	@Override public void setValue(String value) { this.value = value; }
     	@Override public String getValue() { return this.value; }
     },
     EoF;
-    
+
     protected String value;
 	public void setValue(String value) { }
 	public String getValue() { return null; }
--- src/Parser.java
+++ src/Parser.java
@@ -1,12 +1,17 @@
 import java.io.*;
+import java.util.*;
 
 /**
  * Created by markbeussink on 11/29/16.
  */
 public class Parser {
 
-    StringBuilder output;
-    Lexer lxr;
+    private StringBuilder output;
+    private Lexer lxr;
+
+    private ArrayList BodyList;
+
+
 
     public Parser(FileReader inputFile) {
         this.lxr = new Lexer(inputFile);
@@ -14,7 +19,38 @@ public class Parser {
         this.output = new StringBuilder();
     }
 
-    public void parse() {
+    public StringBuilder parse() {
 
+
+        ArrayDeque<ParserState> states = new ArrayDeque<>();    // Create new deque to store lexemes
+        states.add(ParserState.Body);                           // Initial state
+
+        LexerToken tkn;
+
+        // loop over lexemes until EoF
+        do {
+
+            tkn = lxr.lex();
+            ParserState state = states.getLast().transition(tkn);   // transition the last state
+
+            // ToDo: fix this
+            // change to text with
+            while (tkn == null) {
+                System.out.printf("Cannot transition from State: %s, via token: %s",
+                        states.getLast().name(), tkn.name());
+
+                LexerToken txt = LexerToken.Text;
+                txt.setValue(tkn.getValue());
+                state = states.getLast().transition(txt);
+            }
+
+            states.addLast(state);                                  // store the new state
+
+        } while (!states.getLast().equals(ParserState.End.name()));
+
+
+        // translate
+
+        return output;
     }
 }
